5e16b6be83fc8e7d4f1c3db75e7619a54ab6c56b
Dushyant110 <dushyant.sharma.0111@gmail.com>
2024-09-24 02:19:28 +0530

    Installed essential packages

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='bg-red-800'>
+        hey i am red
+      </div>
+    </>
+  )
+}
+
+export default App
